2049f0ba8a0a2f7cb9f2ec423ea07083315dcc6e
Dolapo Adegoju <tichiegoju@gmail.com>
2022-11-29 07:39:52 -0800

    Fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@
 def uppercase(str):
     for i in str:
         if ord(i) >= 65 and ord(i) <= 90:
-            print("{}".format(chr(i)), end="")
+            print("{}".format(chr(ord(i) - 32)), end="")
     print("")
