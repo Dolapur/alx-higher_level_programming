78fa9058ce912dd3b9322b317a87cae40e97b2b7
Dolapo Adegoju <tichiegoju@gmail.com>
2022-12-07 08:23:40 -0800

    completed

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        return ({i: a_dictionary[i] * 2})
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
