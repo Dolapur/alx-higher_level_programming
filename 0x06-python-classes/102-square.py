128a93e6581e937aa31dd706594e11d802efd2bb
Dolapo Adegoju <tichiegoju@gmail.com>
2022-12-21 05:59:48 -0800

    Fixed

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -4,14 +4,13 @@
 
 class Square:
     """defines a square"""
-    
     def __init__(self, size=0):
         """Create a Square
-        Args: 
+        Args:
         size: length of a side of Square
         """
         self.__size = size
-    
+
     @property
     def size(self):
         """The propery of size as the len of a side of Square
@@ -20,14 +19,14 @@ class Square:
         ValueErrorr: if size < 0
         """
         return self.__size
-    
+
     @size.setter
     def size(self, value):
-    if not isinstance(value, int):
-        raise TypeError('size must be an integer')
-    if value < 0:
-        raise ValueError('size must be >= 0')
-    self.__size = value
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     def area(self):
         """Get the area of a Square
@@ -37,13 +36,18 @@ class Square:
 
     def __le__(self, other):
         return self.area() <= other.area()
+
     def __lt__(self, other):
-        return self.area() < other.area()                        
-    def __ge__(self, other):                                
-        return self.area() >= other.area()                        
-    def __ne__(self, other):                    
-        return self.area() != other.area()                        
-    def __gt__(self, other):                                  
-        return self.area() > other.area()                                     
-    def __eq__(self, other):                                 
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __eq__(self, other):
         return self.area() == other.area()
