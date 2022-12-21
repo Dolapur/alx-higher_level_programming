7fc7b7de649aaeed319aef5c5670141fcf42441f
Dolapo Adegoju <tichiegoju@gmail.com>
2022-12-21 05:12:06 -0800

    Fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" class Square that defines a square"""
+
 
 class square:
-    """ Defines a square"""
+    """ class Square that defines a square"""
     def __init__(self, size):
         """Initializing this square class
         Args:
