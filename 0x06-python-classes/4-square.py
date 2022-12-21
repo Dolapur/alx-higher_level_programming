991972f576e85a6b01e449dceccf9a32e28fc759
Dolapo Adegoju <tichiegoju@gmail.com>
2022-12-21 05:17:41 -0800

    Fixed

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" class Square that defines a square"""
 
-class square:
+
+class Square:
     """A class that represents a square"""
     def __init__(self, size=0):
         """Initializing this square class
