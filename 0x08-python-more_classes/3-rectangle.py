9b6fa2082efdee7cfaa0d615284f1908f8da805b
Dolapo Adegoju <tichiegoju@gmail.com>
2023-01-02 13:33:06 -0800

    fixed

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -53,6 +53,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
+
     def __str__(self) -> str:
         """Return the printable representation of the Rectangle.
         Represents the rectangle with the # character.
